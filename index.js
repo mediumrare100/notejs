d219d14d579597262a583ce0420a91c58d70e4b9
mediumrare100 <65351854+mediumrare100@users.noreply.github.com>
2020-05-20 16:42:00 +0200

    Update index.js
    html einfuegen

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('<body><h2>Bundesländer</h2><ol><li>Wien</li><li>Niederösterreich</li><li>Oberösterreich</li><li>Salzburg</li><li>Tirol</li><li>Vorarlberg</li><li>Kärnten</li><li>Steiermark</li><li>Burgenland</li></ol></body>')
 })
 
 app.listen(app.get('port'), function() {
