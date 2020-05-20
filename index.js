c7c69302202d50a68c6410631e0fd4804bdff466
mediumrare100 <65351854+mediumrare100@users.noreply.github.com>
2020-05-20 16:48:30 +0200

    adding path

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express')
-var app = express()
+var path=require('path')
 
+var app = express()
+//extrem wichtig fuer heroku
 app.set('port', (process.env.PORT || 5000))
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
